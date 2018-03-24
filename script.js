c98d0237a9201ac25cce759390d90a6683eb735d
Nicholas Allaire <nick.allaire@gmail.com>
2018-03-24 16:16:10 -0700

    Added panels for each school when clicking school image

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-// window.onscroll = function() {myFunction()};
-
-// var navbar = document.getElementById("nav-bar");
-
-// var sticky = navbar.offsetTop;
-
-// function myFunction() {
-// 	if (window.pageYOffset >= sticky) {
-// 		navbar.classList.add("sticky");
-// 	} else {
-// 		navbar.classList.remove("sticky");
-// 	}
-// }
-
-var navContainer = document.getElementById("nav-bar");
-
-var navItems = document.getElementsByClassName("nav-item");
-for (var i = 0; i < navItems.length; i++) {
-	navItems[i].addEventListener("click", function() {
-		var current = document.getElementsByClassName("active");
-		current[0].className = current[0].className.replace(" active", "");
-		this.className += " active";
+// Function to toggle education panels
+$(document).ready(function() {
+	$("#ucsd-img").click(function() {
+		$("#panel1").toggle();
 	});
-}
+
+	$("#sbcc-img").click(function() {
+		$("#panel2").toggle();
+	});
+});
+
+// Set nav bar item to active as you scroll/click on nav bar item
+var topMenu = $("#nav-bar");
+var topMenuHeight = topMenu.outerHeight() + 50;
+var menuItems = topMenu.find("a");
+var scrollItems = menuItems.map(function() {
+	var item = $($(this).attr("href"));
+	if (item.length) {
+		return item;
+	}
+});
+
+$(window).scroll(function() {
+	var fromTop = $(this).scrollTop() + topMenuHeight;
+	var cur = scrollItems.map(function() {
+		if($(this).offset().top < fromTop)
+			return this;
+	});
+
+	cur = cur[cur.length-1];
+	var id = cur && cur.length ? cur[0].id : "";
+	menuItems.parent().removeClass("active").end().filter("[href='#"+id+"']").parent().addClass("active");
+});
