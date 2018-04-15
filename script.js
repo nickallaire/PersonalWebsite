5e84f31cc86f4fd0f7ab9a2b9d0e9a7fdde60825
Nicholas Allaire <nick.allaire@gmail.com>
2018-04-15 16:11:38 -0700

    Added project information and the ability to add pictures to projects

--- script.js
+++ script.js
@@ -30,6 +30,15 @@ $(document).ready(function() {
 		}
 	});
 
+	$("display-img").click(function() {
+		console.log("GOT HERE");
+	});
+
+	$(".listimg").click(function() {
+		console.log("here");
+	
+	});
+
 	// $("#nav-button").addClass("button-top");
 	// $("#nav-button").removeClass("move");
 
@@ -47,6 +56,10 @@ $(document).ready(function() {
 	} else {
 		$("#nav-bar").show();
 	}
+
+	$(".listimg").click(function() {
+		console.log("HERE");
+	});
 });
 
 // Set nav bar item to active as you scroll/click on nav bar item
@@ -89,63 +102,55 @@ $(window).resize(function () {
 var projects = [
 	{
 		id: "project1",
-		name: "CSE 127 Project",
-		date: "Jan 2017 - Mar 2017",
-		description: "This project is about project 1",
-		language: "Java, C"
+		name: "Event Planner Android Application",
+		date: "September 2016 - December 2016",
+		description: "<h4>Description:</h4><ul><li>Developed an Android Application that integrated Google Maps and Facebook API</li><li>Managed a team of 4 students, delegating tasks in order to meet our milestones</li><li>Built expectations through set iterations using agile process, stand-up meetings, user stories and tasks and implemented via Behavior Driven Development</li><li>Allows users to create/invite friends to events, view items being brought, see event location on maps, and sign up for carpools to an event</li></ul>",
+		language: "Language: Java",
+		images: "<h1>HELLO</h1>"
 	},
 	{
 		id: "project2",
-		name: "CSE 134B Project",
-		date: "Jan 2018 - Mar 2018",
-		description: "This project is about project 2",
-		language: "Java, C"
+		name: "Class Management iOS Application",
+		date: "December 2016 - September 2017",
+		description: "<h4>Description:</h4><ul><li>Swift iOS application that stores class schedule, assignments, and grade distributions</li><li>Able to toggle assignments from a due date to a grade and calculates grade based on graded assignments</li></ul>",
+		language: "Language: Swift",
+		images: "<h1>HELLO</h1>"
 	},
 	{
 		id: "project3",
-		name: "CSE 135 Project",
-		date: "Jan 2019 - Mar 2019",
-		description: "This project is about project 3",
-		language: "Java, C"
+		name: "ISA Developer",
+		date: "April 2017 - June 2017",
+		description: "<h4>Description:</h4><ul><li>Designed a 9 bit instruction set, instruction formats, and a CPU for a single cycle processor</li><li>Designed register file, ALU, fetch unit, and data memory for CPU, complete with a top-level to tie everything together</li><li>Constructed assembler to translate assembly code into machine code for ISA</li><li>Implemented 2 assembly programs(string match and long division) to verify the functionality of the processor</li></ul>",
+		language: "Languages: System Verilog, Python",
+		// images: "<div><img class='display-img' src='Project_Images/CSE141L/CSE141L_1.png' alt='Display img'/></div><div class='listimg-flexbox'><img class='listimg' src='Project_Images/CSE141L/CSE141L_1.png' alt='CSE141L_1'/><img class='listimg' src='Project_Images/CSE141L/CSE141L_2.png' alt='CSE141L_2'/></div>"
+		images: "<h1>HELLO</h1>"
 	},
 	{
 		id: "project4",
-		name: "School Planner Project",
-		date: "Jan 2020 - Mar 2020",
-		description: "This project is about project 4",
-		language: "Java, C"
+		name: "Online Shopping Application",
+		date: "April 2017 - June 2017",
+		description: "<h4>Description:</h4><ul><li>Created an online shopping application for users to buy and sell items</li><li>Uses Java/JSP with PostgreSQL database and Tomcat app server, along with error handling and data validation</li><li>Complete with a sales analytics page for owners, using complex SQL queries and AJAX to filter data quickly</li></ul>",
+		language: "Languages: Java/JSP, PostgreSQL, Tomcat",
+		images: "<div><img class='display-img' src='Project_Images/CSE135/CSE135_1.png' alt='Display img'/></div><div class='listimg-flexbox'><img class='listimg' src='Project_Images/CSE135/CSE135_1.png' alt='CSE135_1'/><img class='listimg' src='Project_Images/CSE135/CSE135_2.png' alt='CSE135_2'/><img class='listimg' src='Project_Images/CSE135/CSE135_3.png' alt='CSE135_3'/><img class='listimg' src='Project_Images/CSE135/CSE135_4.png' alt='CSE135_4'/><img class='listimg' src='Project_Images/CSE135/CSE135_5.png' alt='CSE135_5'/><img class='listimg' src='Project_Images/CSE135/CSE135_6.png' alt='CSE135_6'/><img class='listimg' src='Project_Images/CSE135/CSE135_7.png' alt='CSE135_7'/><img class='listimg' src='Project_Images/CSE135/CSE135_8.png' alt='CSE135_8'/></div>"
 	},
 	{
 		id: "project5",
-		name: "CSE 150 Project",
-		date: "Jan 2021 - Mar 2021",
-		description: "This project is about project 5",
-		language: "Java, C"
+		name: "Micro-bloggin Web Application",
+		date: "September 2017 - December 2017",
+		description: "<h4>Description:</h4><ul><li>Developed a web application that allows users to post onto their message board</li><li>Uses PHP with a PostgreSQL backend database that protects against SQL injection, cross-site scripting, and cross-site forgery attacks</li></ul>",
+		language: "Languages: PHP, SQL",
+		images: "<h1>HELLO</h1>"
 	},
 	{
 		id: "project6",
-		name: "CSE 110 Project",
-		date: "Jan 2021 - Mar 2021",
-		description: "This project is about project 5",
-		language: "Java, C"
-	},
-	{
-		id: "project7",
-		name: "React Project",
-		date: "Jan 2021 - Mar 2021",
-		description: "This project is about project 5",
-		language: "Java, C"
-	},
-	{
-		id: "project8",
-		name: "CSE 141L Project",
-		date: "Jan 2021 - Mar 2021",
-		description: "This project is about project 5",
-		language: "Java, C"
+		name: "Travel Agency Website",
+		date: "January 2018 - March 2018",
+		description: "<h4>Description:</h4><ul><li></li><li></li><li></li></ul>",
+		language: "Languages: HTML, CSS, JavaScript, React, Redux",
+		// images: "<div><img class='display-img' src='Project_Images/CSE134B/CSE134_1.png' alt='Display img'</img></div><div class='listimg-flexbox'><img class='listimg' src='Project_Images/CSE134B/CSE134_1.png' alt='CSE134_1'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_2.png' alt='CSE134_2'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_3.png' alt='CSE134_3'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_4.png' alt='CSE134_4'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_5.png' alt='CSE134_5'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_6.png' alt='CSE134_6'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_7.png' alt='CSE134_7'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_8.png' alt='CSE134_8'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_9.png' alt='CSE134_9'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_10.png' alt='CSE134_10'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_11.png' alt='CSE134_11'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_12.png' alt='CSE134_12'></img><img class='listimg' src='Project_Images/CSE134B/CSE134_13.png' alt='CSE134_13'></img></div>"
+		images: "<h1>HELLO</h1>"
 	}
 ];
-
-console.log(projects);
 
 var clickedProject;
 
@@ -166,7 +171,6 @@ $('.project').hover(function() {
 $(".project").click(function() {
 	var checkDiv = $(this);
 	var i;
-	console.log(checkDiv);
 	if (clickedProject != undefined && checkDiv.attr("id") === clickedProject.attr("id")) {
 		checkDiv.css('background-color', '#eeeddd');
 		$(".project-description").hide();
@@ -180,8 +184,9 @@ $(".project").click(function() {
 			if (projects[i].id === checkDiv.attr("id")) {
 				$(".project-description #project-name").text(projects[i].name);
 				$(".project-description #project-date").text(projects[i].date);
-				$(".project-description #project-description").text(projects[i].description);
+				$(".project-description #project-description").html(projects[i].description);
 				$(".project-description #project-languages").text(projects[i].language);
+				$(".project-description #project-images").html(projects[i].images);
 				$(".project-description").show();
 				checkDiv.css("background-color", "red");
 				clickedProject = checkDiv;
